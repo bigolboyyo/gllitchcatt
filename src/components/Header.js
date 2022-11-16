28b148a4dcae085520bb3324e0f3be5bab7cf8cf
bigolboyyo <wesley.grant94@gmail.com>
2022-11-16 16:37:57 -0500

    header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.scss";
+
+import Container from "@mui/material/Container";
+
+function Header() {
+  return (
+    <Container fluid="true" disableGutters={true} maxWidth={false}>
+      <h1 id="title">
+        <span className="glitch" data-text="GLLITCHCATT">
+          GLLITCHCATT
+        </span>
+      </h1>
+    </Container>
+  );
+}
+
+export default Header;
