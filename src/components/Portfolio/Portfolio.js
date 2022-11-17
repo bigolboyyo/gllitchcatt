17c4e7b78444946de7d3d2ab368803014a650514
bigolboyyo <wesley.grant94@gmail.com>
2022-11-17 01:01:05 -0500

    squid placeholder

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,24 +2,79 @@ import React from "react";
 
 import { ImageList } from "@mui/material";
 import { ImageListItem } from "@mui/material";
-import bkg from "../../images/tmp_bkg.jpg";
+import squid from "../../images/squid32.gif";
+// import bkg from "../../images/tmp_bkg.jpg";
 // import { jsx, css } from "@emotion/react";
 
 const itemData = [
   {
-    img: bkg,
+    img: squid,
     title: "Fun title",
     author: "Cool author",
     cols: 2,
   },
   {
-    img: bkg,
+    img: squid,
     title: "Fun title",
     author: "Cool author",
     cols: 2,
   },
   {
-    img: bkg,
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
+    title: "Fun title",
+    author: "Cool author",
+    cols: 2,
+  },
+  {
+    img: squid,
     title: "Fun title",
     author: "Cool author",
     cols: 2,
@@ -36,8 +91,8 @@ function Portfolio() {
         width: "75vw",
       }}
     >
-      {itemData.map((item) => (
-        <ImageListItem key={item.img} cols={item.cols || 1}>
+      {itemData.map((item, i) => (
+        <ImageListItem key={item.img + i} cols={item.cols || 1}>
           <img src={item.img} alt={item.title} />
         </ImageListItem>
       ))}
