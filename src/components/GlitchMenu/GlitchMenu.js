9ee88b52c61c27c56b2981a24ac15d0b67a028b1
bigolboyyo <wesley.grant94@gmail.com>
2022-11-18 20:31:46 -0500

    grid can't grid

--- src/components/GlitchMenu/GlitchMenu.js
+++ src/components/GlitchMenu/GlitchMenu.js
@@ -28,40 +28,43 @@ function GlitchMenu() {
   }
 
   return (
-    <Container
-      fluid="true"
-      disableGutters={true}
-      maxWidth={false}
-      sx={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: buttonSwitch ? "none" : "space-evenly",
-        paddingBottom: "3vmax",
-        // backgroundImage: `URL(${bkg})`,
-        backgroundColor: "#000",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        position: "relative",
-        zIndex: 100,
-      }}
-    >
-      <div className="stars"></div>
-      <Title />
-      {!buttonSwitch ? (
-        <VerticalButtons
-          buttonSwitch={buttonSwitch}
-          setButtonSwitch={setButtonSwitch}
-        />
-      ) : (
-        <>
-          <HorizontalButtons />
-          <Portfolio />
-        </>
-      )}
-    </Container>
+    <>
+      <Container
+        fluid="true"
+        disableGutters={true}
+        maxWidth={false}
+        sx={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: buttonSwitch ? "none" : "space-evenly",
+          paddingBottom: "3vmax",
+          // backgroundImage: `URL(${bkg})`,
+          backgroundColor: "#000",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          position: "relative",
+          zIndex: 100,
+          // width: "100vw",
+        }}
+      >
+        <div className="stars"></div>
+        <Title />
+        {!buttonSwitch ? (
+          <VerticalButtons
+            buttonSwitch={buttonSwitch}
+            setButtonSwitch={setButtonSwitch}
+          />
+        ) : (
+          <>
+            <HorizontalButtons />
+            <Portfolio />
+          </>
+        )}
+      </Container>
+    </>
   );
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,7 +4,7 @@ import "./Portfolio.css";
 import { ImageList } from "@mui/material";
 import { ImageListItem } from "@mui/material";
 import squid from "../../images/squid32.gif";
-import { Container, flexbox } from "@mui/system";
+// import { Container } from "@mui/system";
 // import bkg from "../../images/tmp_bkg.jpg";
 // import { jsx, css } from "@emotion/react";
 
@@ -13,110 +13,120 @@ const itemData = [
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
   {
     img: squid,
     title: "Fun title",
     author: "Cool author",
-    cols: 2,
+    cols: 1,
   },
 ];
 
 function Portfolio() {
   return (
-    <Container fluid="true">
-      <ImageList
-        cols={4}
-        rowHeight={400}
-        sx={{
-          width: "100vmax",
-          transform: "translateY(-20vmin)",
-          zIndex: "50",
-          position: "absolute",
-          //   margin: 0,
-          left: 0,
-          justifyItems: "center",
-          height: "100%",
-        }}
-        loading="lazy"
-      >
-        {itemData.map((item, i) => (
-          <ImageListItem
-            key={item.img + i}
-            cols={item.cols || 1}
-            sx={
-              {
-                //   left: 0,
-                //   width: "25vmax",
-              }
-            }
-          >
-            <img src={item.img} alt={item.title} />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </Container>
+    // <Container
+    //   fluid="true"
+    //   sx={{
+    //     // width: "100vmax",
+    //     border: "solid red",
+    //   }}
+    // >
+    <ImageList
+      cols={2}
+      rowHeight="auto"
+      gap={100}
+      sx={{
+        width: "-webkit-fill-available",
+        zIndex: "-1",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        justifyItems: "center",
+        height: "-webkit-fill-available",
+        border: "solid green",
+        padding: "20vmin 10vmin 10vmin 10vmin",
+        // gap: "100px",
+      }}
+      loading="lazy"
+    >
+      {itemData.map((item, i) => (
+        <ImageListItem
+          key={item.img + i}
+          cols={item.cols || 1}
+          sx={{
+            //   left: 0,
+            border: "solid red",
+            // paddingTop: "5vmin",
+            //   width: "50vmax",
+            // gap: 100,
+          }}
+        >
+          <img src={item.img} alt={item.title} />
+        </ImageListItem>
+      ))}
+    </ImageList>
+    // </Container>
   );
 }
 
